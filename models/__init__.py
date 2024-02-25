cb6b7ee87b2e251dcd2b4f7ee057ff407641621e
GerryNwaribe <the.star.is.geraldine@gmail.com>
2024-02-25 01:57:24 +0100

    updated init file

--- models/__init__.py
+++ models/__init__.py
@@ -2,7 +2,14 @@
 """ This module contains the base class for all models."""
 
 from models.engine.file_storage import FileStorage
-
+from os import getenv
 """This is the base class for all models."""
-storage = FileStorage()
+
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+
 storage.reload()
